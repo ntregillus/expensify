0b7f449d8535e8006fb7c1fbac927ea2e4f54b23
ntregillus <ntregillus@gmail.com>
2018-08-20 15:22:35 -0600

    setting up Heroku!

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-
+const port = process.env.PORT||3000;
 const publicPath = path.join(__dirname, '..', 'public');
 app.use(express.static(publicPath));
 
@@ -10,6 +10,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('server is up!');
 });

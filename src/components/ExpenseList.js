e3eba18f0b5ccd09f512aa6bddf24eff8b75c380
ntregillus <ntregillus@gmail.com>
2018-08-21 09:46:00 -0600

    adding local storage mock for testing

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import ExpenseListItem from './ExpenseListItem';
+import ExpensesTotal from './ExpensesTotal';
 import selectExpenses from '../selectors/expenses';
 
 export const ExpenseList = (props) => (
--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -1,14 +1,23 @@
-
-
-const expensesReducerDefaultState = [];
+let storedItems = [];
+try
+{ storedItems = JSON.parse(localStorage.getItem('expenses'))||[];
+}catch(e){
+  storedItems = [];
+}
+const expensesReducerDefaultState = [...storedItems];
 const expensesReducer = (state=expensesReducerDefaultState, action) => {
+    const storeState = (expenses) => localStorage.setItem('expenses', JSON.stringify(expenses));
     switch(action.type){
         case 'ADD_EXPENSE':
-        return [...state, action.expense];
+        const result = [...state, action.expense];
+        storeState(result)
+        return result;
         case 'REMOVE_EXPENSE':
-        return state.filter((item)=> item.id != action.id);
+        const r = state.filter((item)=> item.id != action.id);
+        storeState(r)
+        return r;
         case 'EDIT_EXPENSE':
-        return state.map((expense)=>{
+        const newResult = state.map((expense)=>{
             if(expense.id === action.id){
                 return {
                     ...expense, 
@@ -19,6 +28,8 @@ const expensesReducer = (state=expensesReducerDefaultState, action) => {
                 return expense;
             }
         });
+        storeState(newResult);
+        return newResult;
         default:
         return state;
     }

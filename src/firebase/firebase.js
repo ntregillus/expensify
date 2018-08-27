e5b8332b246eb2162e5a92801e0cf107ec302636
Nathan Tregillus <ntregillus@solarcity.com>
2018-08-27 14:01:32 -0600

    fixing location of env files

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -8,7 +8,7 @@ const config = {
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
   };
-console.log('wtf is this ', config);
+  
 firebase.initializeApp(config);
 
 export default firebase.database();
